d61b2d9cdb94bdef5d6a3e0448e49fb00897be03
black <black@localhost.localdomain>
2020-02-04 23:32:55 -0500

    first commit

---
+++ devil.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0a
+mode 100
+cls
+:a
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto a
+
---
+++ replica.py
@@ -0,0 +1,6 @@
+from sys import argv
+import os
+script = argv
+cmd = 'start devil.bat'
+for _ in range(10): 
+	os.system(cmd)
